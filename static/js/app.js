2c0d05a60d253609a0ec2fcf20b308560b8ba1a9
Jeff <jostendorf@gmail.com>
2012-11-22 21:20:13 -0600

    Testing outline of the frontend js file

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,15 @@
 (function () {
+  var rpg;
+
+  function setup() {
+    if (!window.rpg) {
+      return setTimeout(setup, 100);
+    }
+
+    rpg = window.rpg;
+
+    rpg.bind(rpg.EVENTS.READY, function (isLoggedIn) { });
+  };
+
+  setup();
 }());
